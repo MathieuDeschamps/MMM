71f00e9b460afe55fdc51781dd98e2e0a85be742
redeyes361 <redeyes_361@hotmail.com>
2019-01-30 09:50:20 +0100

    add product service

---
+++ app/src/main/java/com/example/boby/mmm/com/example/boby/mmm/model/Product.java
@@ -0,0 +1,4 @@
+package com.example.boby.mmm.com.example.boby.mmm.model;
+
+public class Product {
+}
---
+++ app/src/main/java/com/example/boby/mmm/com/example/boby/mmm/services/ProductService.java
@@ -0,0 +1,18 @@
+package com.example.boby.mmm.com.example.boby.mmm.services;
+
+import com.example.boby.mmm.com.example.boby.mmm.model.Product;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface ProductService {
+
+    public static final String END_POINT = "https://datagram-products-v1.p.mashape.com/storeproduct";
+
+    @GET("/ean/{upc}/")
+    Call<List<Product>> listProduct(@Path("upc") String upc);
+
+}
